a1c1b34fe693dd6789e978f4e199069d35f96f85
BogdanLihinko <borgah12345@gmail.com>
2026-01-21 20:20:59 +0300

    VideoMenu

--- Sister_of_mercy/Assets/Scripts/MenuController.cs
+++ Sister_of_mercy/Assets/Scripts/MenuController.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI; // Обязательно добавьте это пространство имен для работы с UI
 
 public class MenuController : MonoBehaviour
 {
     public GameObject MainMenuSetting;
     public GameObject MainMenuCredits;
+    public GameObject MainMenuPlay;
     public GameObject MainMenu;
-
+    float playTimer = 0f;
+    bool isPlaying = false;
     public void SettingsON()
     {
         MainMenu.SetActive(false);
@@ -29,5 +32,25 @@ public class MenuController : MonoBehaviour
     {
         MainMenu.SetActive(true);
         MainMenuCredits.SetActive(false);
+    }
+
+    public void PlayButton()
+    {
+        MainMenu.SetActive(false);
+        MainMenuPlay.SetActive(true);
+        isPlaying = true;
+    }
+    private void Update()
+    {
+        if (isPlaying) 
+        {
+            playTimer += Time.deltaTime;
+            if(playTimer >= 15f)
+            {
+                playTimer = 0f;
+                SceneManager.LoadScene(1);
+
+            }
+        }
     }
 }
