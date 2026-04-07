cac31bd136f84f2b4e5753323bd9d16a8b96a127
syromolotlyosha77-sudo <syromolotlyosha77@gmail.com>
2026-04-07 19:25:16 +0300

    MainMenu++
    Остались только credits

--- Sister_of_mercy/Assets/Scripts/MenuController.cs
+++ Sister_of_mercy/Assets/Scripts/MenuController.cs
@@ -12,8 +12,9 @@ public class MenuController : MonoBehaviour
     public GameObject MainMenuPlay;
     public GameObject MainMenu;
     public GameObject Background;
-    float playTimer = 0f;
-    bool isPlaying = false;
+    [SerializeField] private float timeToLimit = 15f;
+    private float playTimer;
+    private bool isPlaying = false;
     public void SettingsON()
     {
         MainMenu.SetActive(false);
@@ -39,18 +40,34 @@ public class MenuController : MonoBehaviour
     {
         Background.SetActive(false);
         MainMenuPlay.SetActive(true);
-        isPlaying = true;
+
+        playTimer = 0f; // Сбрасываем таймер перед началом ролика
+        isPlaying = true; // Только теперь запускаем отсчет
     }
+    public void ExitGame()
+    {
+        // Эта строка сработает в скомпилированной игре (.exe)
+        Application.Quit();
+
+        // Эта строка нужна только для проверки в самом редакторе Unity
+        // (так как в редакторе кнопка Quit сама по себе ничего не закроет)
+     #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+     #endif
+
+        Debug.Log("Игра закрыта");
+    }
+
     private void Update()
     {
-        if (isPlaying) 
+        if (isPlaying)
         {
             playTimer += Time.deltaTime;
-            if(playTimer >= 15f)
+            if (playTimer >= timeToLimit)
             {
                 playTimer = 0f;
+                Debug.Log("Пытаюсь загрузить сцену 1...");
                 SceneManager.LoadScene(1);
-
             }
         }
     }
