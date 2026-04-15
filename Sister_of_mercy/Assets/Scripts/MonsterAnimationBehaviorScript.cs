7ecc5aa99311634b5cf3921e528582101e238a8e
PashaNazarov <p7574101@gmail.com>
2026-04-15 17:41:36 +0300

    Revert "AnimationMonster"
    This reverts commit 27a88b24003c1138ce4ae736e044cfc08403f716.

--- Sister_of_mercy/Assets/Scripts/MonsterAnimationBehaviorScript.cs
+++ Sister_of_mercy/Assets/Scripts/MonsterAnimationBehaviorScript.cs
@@ -5,35 +5,45 @@ using UnityEngine;
 public class MonsterAnimationBehaviorScript : MonoBehaviour
 {
     public Animator animator;
-    public bool isWalking = false;
-    public bool isRunning = false;
-    public bool ishitting = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Walk();
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            Run();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            Hit();
+        }
+    }
 
     public void Walk()
     {
         animator.SetBool("isWalking", true);
         animator.SetBool("isRunning", false);
         animator.SetBool("isHitting", false);
-        isWalking = true;
-        isRunning = false;
-        ishitting = false;
     }
     public void Run()
     {
         animator.SetBool("isWalking", false);
         animator.SetBool("isRunning", true);
         animator.SetBool("isHitting", false);
-        isWalking = false;
-        isRunning = true;
-        ishitting = false;
     }
     public void Hit()
     {
         animator.SetBool("isWalking", false);
         animator.SetBool("isRunning", false);
         animator.SetBool("isHitting", true);
-        isWalking = false;
-        isRunning = false;
-        ishitting = true;
     }
 }
