868faf070f9388307d6daf5e88b3d99832501c21
MatveyPlay9123 <gerasimov.m.al@gmail.com>
2026-05-13 18:48:54 +0300

    Menu

--- Sister_of_mercy/Assets/Scripts/EscMenuScript.cs
+++ Sister_of_mercy/Assets/Scripts/EscMenuScript.cs
@@ -1,20 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class EscMenuScript : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
-  
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Ссылка на объект панели меню в Canvas
+    public GameObject menuPanel;
 
-    // Update is called once per frame
+    // Флаг для отслеживания состояния игры
+    private bool isPaused = false;
+
     void Update()
     {
-        
+        // Проверяем нажатие клавиши Escape
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // Метод для закрытия меню и продолжения игры
+    public void Resume()
+    {
+        menuPanel.SetActive(false);     // Скрываем панель меню
+        Time.timeScale = 1f;            // Запускаем игровое время
+        isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked; // Скрываем курсор (опционально)
+        Cursor.visible = false;
+    }
+
+    // Метод для открытия меню и приостановки игры
+    void Pause()
+    {
+        menuPanel.SetActive(true);      // Показываем панель меню
+        Time.timeScale = 0f;            // Замораживаем игровое время
+        isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None; // Освобождаем курсор
+        Cursor.visible = true;                  // Делаем курсор видимым
     }
 }
